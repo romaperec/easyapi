78146ca173fc19eec2d5f00bcad66d21e1420c55
romaperec <cpser621@gmail.com>
2025-01-25 23:37:25 +0300

    0.1.3

--- app/routes/users.py
+++ app/routes/users.py
@@ -16,7 +16,7 @@ async def get_db():
 async def get_all_users(db: AsyncSession = Depends(get_db)):
     response = await db.execute(select(UsersModel))
     users = response.scalars().all()
-    
+
     if not users:
         return HTTPException(status_code=204, detail="DataBase is empty")
     return users
@@ -34,7 +34,10 @@ async def get_user_by_id(user_id: int, db: AsyncSession = Depends(get_db)):
 
 @user_router.post("/create_user/")
 async def create_user(user: UserAddSchema, db: AsyncSession = Depends(get_db)):
-    new_user = UsersModel(name=user.name, description=user.description)
+    name_schema = user.name.strip()
+
+    new_user = UsersModel(name=name_schema, description=user.description)
+
     db.add(new_user)
     await db.commit()
     await db.refresh(new_user)
